29273b5f3773367e0c5dab5b2147be476e100aee
John Carges <johncarges@gmail.com>
2023-11-07 11:02:19 -0500

    adds basic vans page

--- index.jsx
+++ index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import VansList from "./pages/VansList"
+
+import './server'
 
 function App() {
   return (
@@ -10,12 +13,14 @@ function App() {
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
+          <Link to="/vans-list">Vans</Link>
           <Link to="/about">About</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans-list" element={<VansList/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ pages/VansList.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import bgImg from "../assets/images/about-hero.png"
+import { Link } from "react-router-dom"
+
+export default function VansList() {
+    return (
+        <h1>Vans page goes here</h1>
+    )
+}
